eb55813609f4e129a72ae1abeeaafda03d55a6c9
seijimorimoto <angel_seiji@hotmail.com>
2019-11-29 10:57:43 +0000

    Modified WallFollowing behaviour and added conditions in Main.cpp

--- FuzzyLogic/Main.cpp
+++ FuzzyLogic/Main.cpp
@@ -1,6 +1,15 @@
+#include <string>
 #include "Examples.h"
+
+using namespace std;
 
 int main(int argc, char * argv[])
 {
-	return WallFollowObstacleAvoidRobot::main(argc, argv);
+	if (argc == 1)
+		return WallFollowObstacleAvoidRobot::main(argc, argv);
+
+	string argument = argv[1];
+	
+	if (argc == 2 && argument == "FuzzySystemExample")
+		return FuzzySystemExample::main();
 }
